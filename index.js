904a0ce3a94de1f73086e02619f64636513d91ff
Phoo Pyae Pyae Soe <phoopyae111222@gmail.com>
2024-10-28 16:40:14 +0700

    Episode-3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+// globalThis.setTimeout(function(){
+//     console.log('This will run after 30 seconds');
+// },3000)
+let interval = global.setInterval(function () {
+    console.log('This will run every 2 seconds');
+}, 2000)
+
+global.setTimeout(function () {
+    clearInterval(interval);
+}, 3000)
+console.log(__dirname)
+console.log(__filename)
